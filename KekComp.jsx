9fabea1649eaeb35503d43d5304971b1e6bfa50b
casualCoffeeQueen <casualcoffeequeen@gmail.com>
2021-12-20 22:14:07 +0200

    first commit

---
+++ KekComp.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const KekComp = () => {
+    const [value, setValue] = useState('')
+    return (
+        <div>
+            Vot takoi komp
+        </div>
+    );
+};
+
+export default KekComp;
